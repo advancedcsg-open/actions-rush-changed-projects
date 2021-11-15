752d9c7d77c75d8e96163d8a2fe324432a4db11d
Pranav Bhuchhada <Pranav.Bhuchhada@oneadvanced.com>
2021-11-15 10:01:54 +0530

    feat: version fileter implemented

--- dist/main/index.js
+++ dist/main/index.js
@@ -5076,7 +5076,7 @@ const getAllChanges = async ({ rushChangePath, packagePaths, options = {} }) => 
   var changedPackages = await getPackagesFromChanges(rushChangePath)
   // Filter projects by version policy
   console.log("versionPolicy: ", options.versionPolicy)
-  if (options.versionPolicy != null) {
+  if (options.versionPolicy != "") {
     const versionPolicyPackages = packagePaths.filter(project => project.packageVersionPolicy == options.versionPolicy).map(project => project.packageName)
     console.log("versionPolicyPackages :", versionPolicyPackages)
     changedPackages = changedPackages.filter(project => versionPolicyPackages.includes(project))
