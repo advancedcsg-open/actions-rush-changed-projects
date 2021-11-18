2274df2a7f5937298c63965122595d58c1107d69
Pranav Bhuchhada <Pranav.Bhuchhada@oneadvanced.com>
2021-11-18 17:36:25 +0530

    fix: test

--- dist/main/index.js
+++ dist/main/index.js
@@ -5074,13 +5074,6 @@ const getPackagesPaths = async (rushRootPath) => {
 const getAllChanges = async ({ rushChangePath, packagePaths, options = {} }) => {
   // Identify changed packages from change logs
   var changedPackages = await getPackagesFromChanges(rushChangePath)
-  // Filter projects by version policy
-  if (options.versionPolicy != "") {
-    console.log("versionPolicy :", options.versionPolicy)
-    const versionPolicyPackages = packagePaths.filter(project => project.packageVersionPolicy == options.versionPolicy).map(project => project.packageName)
-    console.log("versionPolicyPackages :", versionPolicyPackages)
-    changedPackages = changedPackages.filter(project => versionPolicyPackages.includes(project))
-  }
   // Start off with the changed packages
   const allChanges = [...changedPackages]
 
@@ -5103,6 +5096,13 @@ const getAllChanges = async ({ rushChangePath, packagePaths, options = {} }) => 
         allChanges.push(packageName)
       }
     }
+  }
+  // Filter projects by version policy
+  if (options.versionPolicy != "") {
+    console.log("versionPolicy :", options.versionPolicy)
+    const versionPolicyPackages = packagePaths.filter(project => project.packageVersionPolicy == options.versionPolicy).map(project => project.packageName)
+    console.log("versionPolicyPackages :", versionPolicyPackages)
+    allChanges = allChanges.filter(project => versionPolicyPackages.includes(project))
   }
   return allChanges
 }
